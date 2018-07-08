1987a647266b077bb823324e28408b6cf17ba125
Viacheslav <zslavman@mac-admin.local>
2018-07-08 03:47:41 +0300

    Initial Commit

---
+++ SpacerGame/GameScene.swift
@@ -0,0 +1,26 @@
+//
+//  GameScene.swift
+//  SpacerGame
+//
+//  Created by Viacheslav on 08.07.18.
+//  Copyright © 2018 HomeMade. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    
+
+    override func didMove(to view: SKView) {
+        let spaceShip = SKSpriteNode(imageNamed: "spaceShip")
+        addChild(spaceShip)
+    }
+
+
+
+
+
+
+}
