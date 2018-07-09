01c602c6bbac2033bfc02e46d75c0944e7157f24
Viacheslav <zslavman@mac-admin.local>
2018-07-10 01:14:14 +0300

    add productivity improvement & bit mask working

--- SpacerGame/GameScene.swift
+++ SpacerGame/GameScene.swift
@@ -16,13 +16,18 @@ class GameScene: SKScene {
     private let h = UIScreen.main.bounds.size.height
     
     private let ship_speed:CGFloat = 600    // поинтов в секунду
-    private let asterPerSecond:Double = 7   // кол-во астероидов в сек
+    private let asterPerSecond:Double = 2   // кол-во астероидов в сек
     
-    
+    // идентификаторы столкновений (битовые маски)
+    private let chipCategory:UInt32     = 0x1 << 0 // 0000..01
+    private let asterCategory:UInt32    = 0x1 << 1 // 0000..10
     
     override func didMove(to view: SKView) {
         
         self.removeAllChildren()
+        
+        physicsWorld.contactDelegate = self
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -0.8) // гравитация - вектор, направленный сверху-вниз с ускорением -9,8
         
         // бэкграунг сцены
         let stageBacking = SKSpriteNode(imageNamed: "background")
@@ -34,10 +39,16 @@ class GameScene: SKScene {
         
         // космич. корабль
         spaceShip = SKSpriteNode(imageNamed: "picSpaceShip")
-        spaceShip.position = CGPoint(x: 200, y: 200)
+        spaceShip.position = CGPoint(x: w/2, y: spaceShip.frame.size.height/2 + 10)
         spaceShip.physicsBody = SKPhysicsBody(texture: spaceShip.texture!, size: spaceShip.size)
         spaceShip.physicsBody?.isDynamic = false // гравитация не должна утягивать корабль вниз
         spaceShip.zPosition = 1
+        
+        // определяем с кем корабль будет сталкиваться
+        spaceShip.physicsBody?.categoryBitMask = chipCategory
+        spaceShip.physicsBody?.collisionBitMask = asterCategory
+        spaceShip.physicsBody?.contactTestBitMask = asterCategory // на что мы должны получать уведомление
+        
         addChild(spaceShip)
         
         
@@ -79,8 +90,8 @@ class GameScene: SKScene {
             let dist = distanceCalc(a: spaceShip.position, b: touchLocation)
             let time = timeToTravelDistance(distance: dist, speed: ship_speed)
             
-            print("distance = \(dist)")
-            print("time = \(time)")
+//            print("distance = \(dist)")
+//            print("time = \(time)")
             let moveAction = SKAction.move(to: touchLocation, duration: time)
             
             spaceShip.run(moveAction)
@@ -105,9 +116,33 @@ class GameScene: SKScene {
         
         // назначаем астероиду физическое тело для взаимодействия. Метод ниже определяет физ.тело на основе прозрачности слоя
         asteroid.physicsBody = SKPhysicsBody(texture: asteroid.texture!, size: asteroid.size)
+        asteroid.name = "asteroid1"
+        
+        asteroid.physicsBody?.categoryBitMask = asterCategory
+        asteroid.physicsBody?.collisionBitMask = chipCategory
+        asteroid.physicsBody?.contactTestBitMask = chipCategory
         
         return asteroid
     }
+    
+    
+    // метод, срабатываемый после просчета физики в каждом кадре ENTERFRAME
+    override func didSimulatePhysics() {
+        
+        // метод перебирает все ноды сцены c указанным именем;
+        // stop - прекращает работу метода/перебора нодов
+        enumerateChildNodes(withName: "asteroid1") {
+            (node:SKNode, stop:UnsafeMutablePointer<ObjCBool>) in
+            if node.position.y < -20 {
+                node.removeFromParent()
+            }
+        }
+    }
+    
+    
+    
+    
+    
     
     
     
@@ -148,10 +183,37 @@ class GameScene: SKScene {
         return TimeInterval(time)
     }
     
-    
-    
-
-
 }
+    
+
+
+extension GameScene: SKPhysicsContactDelegate{
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        print("\(contact.bodyA.node?.name) врезался в наш кораблик, йопта!")
+    }
+    
+    func didEnd(_ contact: SKPhysicsContact) {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
